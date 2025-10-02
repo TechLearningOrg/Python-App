cc48d9f2bf48bd71494f4d7c5f41deffa0cf1764
Prince Robin <princerobinraj@gmail.com>
2025-10-02 19:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Prince Robin"
+    return "Hello World from Python flask app"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
